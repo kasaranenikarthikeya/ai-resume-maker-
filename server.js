85518f039f21012b3916f8d4ebe4b851f36bb869
kasaranenikarthikeya <149353084+kasaranenikarthikeya@users.noreply.github.com>
2025-04-22 00:05:18 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors({ origin: "https://ai-resume-client.onrender.com" }));
+app.use(express.json());
+
+// Routes
+app.use("/api", routes);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("AI Resume Builder Backend");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
